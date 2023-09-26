b32349dccbf9ebb947c4e714704b8dfe630f2b1f
sachariase99 <sachariase99@hotmail.com>
2023-09-26 09:06:28 +0200

    s

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const fornavn = "Jannick";
+const efternavn = "Sachariasen";
+const fuldtNavn = fornavn + " " + efternavn;
+console.log(fuldtNavn);
+
+const brugernavn = "Sachariasen";
+const velkomstbesked = "Velkommen, " + brugernavn + "!";
+console.log(velkomstbesked);
+
+const by = "Storvorde";
+const postnummer = "9280";
+const adresse = by + ", " + postnummer;
+console.log(adresse);
+
+const ferieDestination = "Frankrig";
+const feriePlan = "Jeg planlægger at tage til " + ferieDestination + " i næste uge.";
+console.log(feriePlan);
+
+const produktNavn = "CD";
+const produktPris = "15";
+const produktBesked = "Det valgte produkt er " + produktNavn + ", og det koster " + produktPris + " dollars.";
+console.log(produktBesked);
